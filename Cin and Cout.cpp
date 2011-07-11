8b9ec7fbe15998849afe76cd7a4d01ef1fa29107
Liam World <Deathray75@hotmail.co.uk>
2011-07-11 18:20:29 +0100

    Cin And Cout Usage

---
+++ Cin and Cout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    // ask the user the number to enter
+    cout << "enter a number" << endl;
+    // Declares x
+    int x;
+    // X is now equal to the number user entered
+    cin >> x;
+    cout << "you entered " << x << endl;
+    return 0;
+}
