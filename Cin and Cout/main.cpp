77da1063fdc449ca31866ac649ec2d7c0222de1e
Liam World <Deathray75@hotmail.co.uk>
2011-07-12 17:03:46 +0100

    LOT OF WORK TO DO!!!!!!!!!!!!!!!!!!!!!

---
+++ Cin and Cout/main.cpp
@@ -0,0 +1,20 @@
+
+// tells the compiler what libary it is
+#include <iostream>
+//always needed
+int main()
+{
+    //Small bits of the libary
+    using namespace std;
+    //declare X
+    int x;
+    // show on console Please enter a number: and move to new line
+    cout << "Please enter a number: " << endl;
+    // input is = to x
+    cin >> x;
+    // show on screen X and move to new line
+    cout << "you entered " << x << endl;
+    // return 0
+    return 0;
+
+}
